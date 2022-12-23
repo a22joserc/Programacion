dade39ed85da9ccd54a85d5cf03e22029b5a49bb
a22joserc <a22joserc@iessanclemente.net>
2022-12-23 18:47:44 +0100

    Primera Prueba

---
+++ PruebaJava/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author jvramos
+ *
+ */
+module HolaMacaco {
+}
---
+++ PruebaJava/src/prueba/HolaMacaco.java
@@ -0,0 +1,12 @@
+package prueba;
+
+public class HolaMacaco {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "Hola Macaco!!!!";
+		System.out.println(str);
+
+	}
+
+}
